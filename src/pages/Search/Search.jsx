953867c7b9cb16761f2c11d5da74b81572f55cc9
ehsanfuad <ehsanfuad1402@gmail.com>
2023-06-17 11:36:12 +0430

    first commit

---
+++ src/pages/Search/Search.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Container } from "@mui/material";
+import { useParams } from "react-router-dom";
+
+function Search() {
+  const params = useParams();
+  const categorySlug = params.category;
+  const categoryId = params.id;
+
+  return <Container maxWidth="xl"></Container>;
+}
+
+export default Search;
